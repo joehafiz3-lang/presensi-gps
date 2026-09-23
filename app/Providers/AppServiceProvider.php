df73d212ef54d96a432b941cd9931c937de56667
Jauhari <m.jauhari1986@gmail.com>
2026-09-23 16:02:36 +0700

    Fix: merapikan struktur kurung kurawal di AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
-    }
-}
+        // Tambahkan baris kode di bawah ini untuk Vercel:
+        if (config('app.env') === 'production') {
+            $array = [
+                '/tmp/storage/framework/views',
+                '/tmp/storage/framework/cache',
+                '/tmp/storage/framework/sessions'
+            ];
+            foreach ($array as $path) {
+                if (!file_exists($path)) {
+                    mkdir($path, 0777, true);
+                }
+            }
+        }
+    } // <-- Kurung kurawal penutup untuk fungsi boot()
+} // <-- Kurung kurawal penutup untuk class AppServiceProvider
